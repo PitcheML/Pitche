bb1672277e8d13b77b2f67b04d482b71321e2eae
Christopher Trinh <Chris.trinh@live.com>
2020-10-08 14:44:04 -0400

    Refactored: functional component to class component for video, created Functional VideoFeed Component

--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome, Video, Results} from './components'
+import {Login, Signup, UserHome, Video, Results, VideoFeed} from './components'
 import {me} from './store'
 
 /**
@@ -21,7 +21,7 @@ class Routes extends Component {
         {/* Routes placed here are available to all visitors */}
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
-        <Route path="/video" component={Video} />
+        <Route path="/video" component={VideoFeed} />
         <Route path="/results" component={Results} />
         {isLoggedIn && (
           <Switch>
